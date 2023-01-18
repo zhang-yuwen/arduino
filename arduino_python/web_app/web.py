bf9e664a7fead4e322298df948057c1d4e42b595
Yuwen <yuwen@pennyapp.com>
2023-01-18 17:27:33 -0500

    refactor web app

--- arduino_python/web_app/web.py
+++ arduino_python/web_app/web.py
@@ -1,37 +1,31 @@
 from flask import Flask, render_template, redirect
-from serial_money import serial_io, control, led_state, led_on, led_off
-import threading
+from arduino import LED
 from time import sleep
 
-thread1 = threading.Thread(target=serial_io)
-thread2 = threading.Thread(target=control)
-
-thread1.start()
-# thread2.start()
+led=LED()
 
 app = Flask(__name__)
 
 @app.route("/")
 def status():
-    print(f'Led state: {led_state}')
-    if led_state:
-        return render_template('index.html', message = led_state[-1])
+    print(f'Led state: {led.led_state}')
+    if led.led_state:
+        return render_template('index.html', message = "LED lights are on.")
     else:
-        return render_template('index.html', message = "empty")
-    #return led_state[-1]
+        return render_template('index.html', message = "LED lights are off.")
 
 @app.route("/on", methods = ['POST'])
 def on():
-    led_on()
+    led.on()
     sleep(0.1)
     return redirect("/", code=302)
 
 
 @app.route("/off", methods = ['POST'])
 def off():
-    led_off()
+    led.off()
     sleep(0.1)
     return redirect("/", code=302)
 
-# flask --app web_app/web.py run --port 8080
+# flask --app web.py run --port 8080
 # ngrok http 8080 
